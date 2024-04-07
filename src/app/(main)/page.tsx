3bd9b38794c666ae6ed73962db4d0f32d8a43ed0
Manan Gulati <manangulati9@gmail.com>
2024-04-07 23:18:44 +0530

    initial commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,5 +3,5 @@ import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
-  return <div className="h-screen">Hello world</div>
+  return <div>Hello world</div>
 }
