7eeeb95e97e9b39c7c58cfd6d961feb40e608723
Manan Gulati <manangulati9@gmail.com>
2024-04-13 00:44:02 +0530

    fixed conflicts

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
-/*
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 await import("./src/env.js");
 
 const config = {
 	reactStrictMode: true,
 	images: {
 		remotePatterns: [{ hostname: "gtsqetcjzlgmnjxsbmns.supabase.co" }],
+		formats: ["image/avif", "image/webp"],
 	},
 };
 
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -2,7 +2,6 @@ import "server-only";
 
 import { headers } from "next/headers";
 import { cache } from "react";
-
 import { createCaller } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
@@ -11,12 +10,12 @@ import { createTRPCContext } from "@/server/api/trpc";
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+	const heads = new Headers(headers());
+	heads.set("x-trpc-source", "rsc");
 
-  return createTRPCContext({
-    headers: heads,
-  });
+	return createTRPCContext({
+		headers: heads,
+	});
 });
 
 export const api = createCaller(createContext);
