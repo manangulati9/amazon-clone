95f321d4ee92b3ed30c520c45eec86a914f00739
Manan Gulati <manangulati9@gmail.com>
2024-04-13 00:49:47 +0530

    fixed lint file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-<<<<<<< HEAD
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		project: true,
@@ -40,45 +39,3 @@ const config = {
 	},
 };
 module.exports = config;
-=======
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
-  },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
-  ],
-  "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
-module.exports = config;
->>>>>>> b5f2c0e (initial commit)
